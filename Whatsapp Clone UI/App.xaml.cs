9558db5c9264c9c66a7a35c57e7b87396860feb2
etudiant <etudiant@MB-R-110001001.local>
2022-12-11 01:25:13 +0100

    Mise en place de la structure des Pages

--- Whatsapp Clone UI/App.xaml.cs
+++ Whatsapp Clone UI/App.xaml.cs
@@ -8,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new NavigationPage(new NewPage1());
+		MainPage = new NavigationPage(new MainView());
 	}
 }
--- Whatsapp Clone UI/Views/NewPage1.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Whatsapp_Clone_UI.Views;
-
-public partial class NewPage1 : ContentPage
-{
-	public NewPage1()
-	{
-		InitializeComponent();
-	}
-}
